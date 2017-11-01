3c51f030b42ac371551ffcf11160d84840b6bced
Charence Wang <wangyl1021@vip.qq.com>
2017-11-01 23:11:35 +0800

    responsive menu

--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -1,5 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 
+class MenuItem {
+  displayName: string;
+  url: string
+}
+
 @Component({
   selector: 'nav.app-menu',
   templateUrl: './menu.component.html',
@@ -7,7 +12,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MenuComponent implements OnInit {
 
-  constructor() { }
+  menuItems: Array<MenuItem>;
+
+  constructor() {
+    this.menuItems = [
+      {displayName: "Home", url: "/products"},
+      {displayName: "Account", url: "/account"},
+      {displayName: "Pages", url: "/pages"},
+      {displayName: "Computers", url: "/products/computers"},
+      {displayName: "Contact", url: "/contact"}
+    ]
+  }
 
   ngOnInit() {
   }
