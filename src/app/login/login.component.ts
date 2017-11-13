daf1d7746d08db0e84fe33434cd1850931679506
Charence Wang <wangyl1021@vip.qq.com>
2017-11-13 23:57:34 +0800

    Update animation

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ElementRef } from '@angular/core';
 import { NgForm } from '@angular/forms';
 
 import { UserCredentials, UserInfo } from '../../models/userinfo';
@@ -8,6 +8,7 @@ import { SessionService } from '../session.service';
 import { Router } from '@angular/router';
 import { ShoppingCartService } from "../shopping-cart.service";
 
+
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
@@ -16,8 +17,10 @@ import { ShoppingCartService } from "../shopping-cart.service";
 export class LoginComponent implements OnInit {
 
   constructor(private router: Router,
+    private elementRef: ElementRef,
     private userSvc: UserService,
-    private sessionSvc: SessionService,private shoppingCartSvc:ShoppingCartService) { }
+    private sessionSvc: SessionService,
+    private shoppingCartSvc:ShoppingCartService) { }
 
   ngOnInit() {
     this.sessionSvc.loginUserInfo = null;
@@ -25,16 +28,22 @@ export class LoginComponent implements OnInit {
   }
 
   doLogin(form: NgForm): void {
+    let $loginBtn = $('button[type=\'submit\']', this.elementRef.nativeElement);
+    
     if (!form.valid) {
       alert("Please input the User Name and Password.");
       return;
     }
+    
+    (<any>$loginBtn).button('loading');
     this.userSvc.login(form.value.username, form.value.password)
       .then((userInfo: UserInfo) => {
+        (<any>$loginBtn).button('reset');
         this.sessionSvc.loginUserInfo = userInfo;
         this.router.navigate(['products']);
       }).catch((errorMsg: string) => {
         //TODO: show error message popup
+        (<any>$loginBtn).button('reset');
         alert(errorMsg);
       });
   }
